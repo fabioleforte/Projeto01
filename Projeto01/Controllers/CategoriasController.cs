4ccedbeac911f3b2e75f67cae6ae0f1143f1c02f
fabioleforte <fabio.leforte@gmail.com>
2016-09-21 09:03:19 -0300

    Add project files.

---
+++ Projeto01/Controllers/CategoriasController.cs
@@ -0,0 +1,73 @@
+﻿using Projeto01.Contexts;
+using Projeto01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto01.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(context.Categorias.);
+        }
+
+        //GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        //GET: Edit
+        public ActionResult Edit(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaId == id).First());
+        }
+
+        //POST: Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+
+        //GET: Details
+        public ActionResult Details(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaId == id).First());
+        }
+
+        //GET: Delete
+        public ActionResult Delete(long id)
+        {
+            return View(categorias.Where(m => m.CategoriaId == id).First());
+        }
+
+        //POST: Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Categoria categoria)
+        {
+            categorias.Remove(categorias.Where(c => c.CategoriaId == categoria.CategoriaId).First());
+            return RedirectToAction("Index");
+        }
+    }
+}
